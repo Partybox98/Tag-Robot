a1235d32491717eebf522f065e303c63a5c43e74
Partybox98 <kirbymmo@gmail.com>
2024-09-13 14:31:28 -0700

    still stuck in the god damn center

--- game/players/tagger.py
+++ game/players/tagger.py
@@ -1,35 +1,71 @@
 from player_controls import Player
+from pygame import Vector2
 import pygame
 
 class Tagger(Player):
   cornered = False
   cornered_dis = 100 # find correct value
   HEIGHT = WIDTH = 1000
+  centered = False
+  run_pos = ""
   
   def corner1(self): # fix movement to corner better
     run = self.other_player.pos
     
-    if run.x < self.pos.x:
+    r_left = False
+    r_up = False
+    
+    if run.x < self.WIDTH/2:
+      r_left = True
+    if run.y < self.WIDTH/2:
+      r_up = True
+      
+    r_quad = ""
+    if (r_left):
+      r_quad += "left"
+    else:
+      r_quad += "right"
+    r_quad += " "
+    if (r_up):
+      r_quad += "up"
+    else:
+      r_quad += "down"
+    return r_quad
+      
+      
+      
+    # change to give directions for cornering
+    
+    
+    
+    # if (run.x < self.cornered_dis and run.y < self.cornered_dis):
+    #   return "TL"
+    # elif (run.x < self.cornered_dis and run.y > self.HEIGHT - self.cornered_dis):
+    #   return "BL"
+    # elif (run.x > self.HEIGHT - self.cornered_dis and run.y < self.cornered_dis):
+    #   return "TR" 
+    # elif (run.x > self.HEIGHT - self.cornered_dis and run.y < self.HEIGHT - self.cornered_dis):
+    #   return "BR"
+    # else:
+    #   return ""
+  
+  def center(self): # fix pls
+    dest = Vector2(self.WIDTH/2, self.HEIGHT/2)
+    if dest.x < self.pos.x:
       self.move_left()
     else:
       self.move_right()
-    if run.y < self.pos.y:
+    # important to note that smaller y == higher up.  
+    if dest.y < self.pos.y:
       self.move_up()
     else:
       self.move_down()
-    # change to give directions for cornering
-    if (run.x < self.cornered_dis and run.y < self.cornered_dis):
-      return True, "TL" # Cornered = True, run_pos = top left / "TL"
-    elif (run.x < self.cornered_dis and run.y > self.HEIGHT - self.cornered_dis):
-      return True, "BL" # Cornered = True, run_pos = bottom left / "BL"
-    elif (run.x > self.HEIGHT - self.cornered_dis and run.y < self.cornered_dis):
-      return True, "TR" # Cornered = True, run_pos = top right / "TR"
-    elif (run.x > self.HEIGHT - self.cornered_dis and run.y < self.HEIGHT - self.cornered_dis):
-      return True, "BR" # Cornered = True, run_pos = bottom right / "BR"
+    if (self.pos == dest):
+      return True
     else:
-      return False, ""
-  
-  
+      return self.centered
+
+
   
   
   
@@ -37,12 +73,19 @@ class Tagger(Player):
   # called once every frame.
   def act(self):
     
+    
+    
     # try to move toward runner
     run = self.other_player.pos
-    if (self.cornered == False):
-      self.cornered,run_pos = self.corner1()
+    
+    if (self.centered):
+      if (self.run_pos == ""):
+        self.run_pos = self.corner1()
+      else:
+        self.cornered = True
+        print(f'Cornered, pos:{self.run_pos}')
     else:
-      print(f'Cornered, pos:{run_pos}')
+      self.centered = self.center()
     
 
 

bbcba41afe09802b8728f536a9c4e00701124ed3
Partybox98 <kirbymmo@gmail.com>
2024-09-13 15:20:30 -0700

    waiting for prediction chase func

--- game/players/tagger.py
+++ game/players/tagger.py
@@ -4,37 +4,55 @@ import pygame
 
 class Tagger(Player):
   HEIGHT = WIDTH = 1000
-  centered = False
-  gapv = False
+  state = 0 # 0 is close gap, 1 is wall, 2 is chase?
   
-  def center(self):
-    dest = Vector2(self.WIDTH/2, self.HEIGHT/2)
-    if dest.x < self.pos.x:
-      self.move_left()
-    else:
-      self.move_right()
-    if dest.y < self.pos.y:
-      self.move_up()
-    else:
-      self.move_down()
-    # if (self.pos.x < (self.WIDTH/2 + 10) and self.pos.x > (self.WIDTH/2 - 10) and self.pos.y < (self.HEIGHT/2 + 10) and self.pos.y > (self.HEIGHT/2 - 10)):
-    if (self.within(self.pos.x,self.WIDTH/2,10) and self.within(self.pos.y,self.HEIGHT/2,10)):
-      return True
 
-  def gap(run):
-    if (run.pos.x == 0):
-      return True
+  def gap(self,run,dis):
+    if (self.within(self.pos.x,run.x,dis) and self.within(self.pos.y,run.y,dis)):
+      return 1
+    else:
+      if run.x < self.pos.x:
+        self.move_left()
+      else:
+        self.move_right()
+      if run.y < self.pos.y:
+        self.move_up()
+      else:
+        self.move_down()
+      return 0
 
-  
+
+  def wall(self,run,dis):
+    if (self.within(self.pos.x,run.x,dis) and self.within(self.pos.y,run.y,dis)):
+      if ((self.pos.x - run.x) < (self.pos.y - run.y)): #figure out if it should block x or y
+        if run.x < self.pos.x:
+          self.move_left()
+        else:
+          self.move_right()
+      else:
+        if run.y < self.pos.y:
+          self.move_up()
+        else:
+          self.move_down()
+      if (self.within(self.pos.x,run.x,dis/2) and self.within(self.pos.y,run.y,dis/2)):
+        return 2
+      else:
+        return 1
+    else:
+      return 0
   # called once every frame.
   def act(self):
-    
+    dis = 220
     run = self.other_player.pos
     
-    if (self.centered):
-      self.gapv = self.gap(run)
+    if (self.state == 0):
+      self.state = self.gap(run,dis)
+    elif (self.state == 1):
+      self.state = self.wall(run,dis+20)
+    elif (self.state == 2):
+      print("done") # make a prediction chase function to finish catching it
     else:
-      self.centered = self.center()
+      print("error")
     
 
   
